7f1634db8c180b41a9b911bc326ae9e9273b9968
Andrew Doumont <andrew.doumont@gmail.com>
2022-07-17 13:24:21 -0600

    update

--- src/components/templates/blog-posts.tsx
+++ src/components/templates/blog-posts.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import { Link } from "gatsby";
 import Layout from "../layout";
 require(`katex/dist/katex.min.css`);
-require("prismjs/themes/prism-solarizedlight.css");
 
 export default ({ pageContext }: { pageContext: any }) => {
   const post = pageContext;
