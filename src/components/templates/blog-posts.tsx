b0c60206021040e0d328c0e133f1b1b19928c4b4
Andrew Doumont <andrew.doumont@gmail.com>
2022-07-17 12:57:39 -0600

    updated styles

--- src/components/templates/blog-posts.tsx
+++ src/components/templates/blog-posts.tsx
@@ -12,25 +12,32 @@ export default ({ pageContext }: { pageContext: any }) => {
       <div className="blog-container">
         <h1>{post.frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
-      </div>
-      <div>
-        {post.next && (
-          <Link to={post.next.fields.slug}>
-            &#8592;
-            {`${post.next.frontmatter.title}`}
-          </Link>
-        )}
-        <div></div>
-        {post.previous && (
-          <div>
-            <Link to={post.previous.fields.slug}>
-              {`${post.previous.frontmatter.title}`}
-            </Link>
-            &#8594;
+        <div>
+          <div className="other-articles">
+            {post.next && (
+              <Link className="link-underline" to={post.next.fields.slug}>
+                &#8592;
+                {`${post.next.frontmatter.title}`}
+              </Link>
+            )}
+            <div></div>
+            {post.previous && (
+              <Link className="link-underline" to={post.previous.fields.slug}>
+                {`${post.previous.frontmatter.title}`}
+                &#8594;
+              </Link>
+            )}
           </div>
-        )}
+          <h3 className="divider"></h3>
+          <footer>
+            <div>
+              © {new Date().getFullYear()}, Built with
+              {` `}
+              <a href="https://www.gatsbyjs.org">Gatsby</a>
+            </div>
+          </footer>
+        </div>
       </div>
-      <div></div>
     </Layout>
   );
 };
